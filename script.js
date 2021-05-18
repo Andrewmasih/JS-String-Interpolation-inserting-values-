efc26b8b1e93434a434e440def79b23cc6ccecda
Andrew Masih <armasih@outlook.com>
2021-05-18 23:38:04 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let leftSide = 10;
+let rightSide = 20;
+
+
+/* the old way would be to insert values of 2 strings into a new string, notice the plus operator and how we had to split our main string into serval chunks */
+console.log ("left side is equal to " + leftSide + ", and right side is equal to " + rightSide + ", happy?");
+
+
+/* now we insert the values directly into Template Literals using ${} */
+console.log (`leftSide is equal to ${leftSide} and right side is equal to ${rightSide},this is easier, im so happy!` );
+
